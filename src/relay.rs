c693c93af2ab2f8a9128a4d5da8e3fa5d3e0a3eb
Jake Stambaugh <jake.stambaugh@firaxis.com>
2022-06-23 14:10:16 -0400

    Add disconnect feature
    When a client closes their window, the player id is added back to the pool.
    Their read and write tasks still exist, they will need to return when
    they detect that the player has disconnected.

--- src/relay.rs
+++ src/relay.rs
@@ -17,14 +17,37 @@ use crate::engine::{
     turn,
 };
 
-pub struct RelayServer {
-    input_tx_handle: mpsc::UnboundedSender<GameInputEvent>,
-    broadcast_tx_handle: broadcast::Sender<GameLogEvent>,
-
+/// This is the data about the server that lives inside the async RelayServer task. It includes
+/// metadata about the server, mostly related to player connection and identification info.
+struct ServerState {
     /// When a new relay is created, this field is filled with all of the player ids that the
     /// server could support. Whenever a new player connects to the server, one player id is removed
     /// from the pool and assigned to a player task.
     available_player_slots: Vec<PlayerId>,
+}
+
+impl ServerState {
+    pub fn new() -> Self {
+        Self {
+            available_player_slots: vec![PlayerId::A, PlayerId::B],
+        }
+    }
+
+    pub fn claim_player_slot(&mut self) -> Option<PlayerId> {
+        self.available_player_slots.pop()
+    }
+}
+
+pub struct RelayServer {
+    /// This channel is used to send GameInputEvents from the `ws_read` tasks (incoming websocket messages) to the central
+    /// server and state machine. When a new RelayServer is constructed, the rx half of this channel is sent to the `run_server` task
+    input_tx_handle: mpsc::UnboundedSender<GameInputEvent>,
+
+    /// When the central server and state machine have a new event that needs to be sent to the clients, it is sent over this channel.
+    /// Whenever a new `ws_write` task is created, it subscribes to this channel, creating another rx handle.
+    broadcast_tx_handle: broadcast::Sender<GameLogEvent>,
+
+    server_state: Arc<Mutex<ServerState>>,
 
     // This is the task that runs the relay server logic
     _task: JoinHandle<()>,
@@ -36,16 +59,17 @@ impl RelayServer {
         let (btx, _) = broadcast::channel(32);
         let game_log = GameLog::new(btx.clone());
         let game_state = init_gamestate(game_log);
+        let server_state = Arc::new(Mutex::new(ServerState::new()));
         Self {
             input_tx_handle: tx,
             broadcast_tx_handle: btx.clone(),
-            available_player_slots: vec![PlayerId::A, PlayerId::B],
-            _task: tokio::spawn(run_server(rx, btx, game_state)),
+            server_state: server_state.clone(),
+            _task: tokio::spawn(run_server(rx, btx, game_state, server_state.clone())),
         }
     }
 
-    fn claim_player_slot(&mut self) -> Option<PlayerId> {
-        self.available_player_slots.pop()
+    async fn claim_player_slot(&mut self) -> Option<PlayerId> {
+        self.server_state.lock().await.claim_player_slot()
     }
 
     fn run_player(&mut self, socket: WebSocket, id: PlayerId) {
@@ -67,13 +91,24 @@ impl Default for RelayServer {
 async fn run_server(
     mut input_events: mpsc::UnboundedReceiver<GameInputEvent>,
     _output_events: broadcast::Sender<GameLogEvent>,
-    mut state: GameState,
+    mut game_state: GameState,
+    server_state: Arc<Mutex<ServerState>>,
 ) {
-    while !state.deck_is_empty() {
-        let _input = input_events.recv().await;
-
-        // TODO: figure out how to communicate back to the owning server. This might be impossible, so player count may have to live elsewhere.
-        turn(&mut state);
+    while !game_state.deck_is_empty() {
+        if let Some(_input) = input_events.recv().await {
+            if _input._close {
+                // This is effectively "disconnecting" this player and returning their id to the pool of available ids
+                server_state
+                    .lock()
+                    .await
+                    .available_player_slots
+                    .push(_input.player);
+            }
+            // TODO: figure out how to communicate back to the owning server. This might be impossible, so player count may have to live elsewhere.
+            turn(&mut game_state);
+        } else {
+            tracing::warn!("Got a None input event from the input_events channel");
+        }
     }
 }
 
@@ -81,7 +116,7 @@ async fn run_server(
 pub async fn handle_socket(socket: WebSocket, relay: Arc<Mutex<RelayServer>>) {
     // Assign to player
     let mut r = relay.lock().await;
-    match r.claim_player_slot() {
+    match r.claim_player_slot().await {
         Some(id) => r.run_player(socket, id),
         None => {
             // Send disconnect message
